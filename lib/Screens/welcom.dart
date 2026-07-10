e833ce22f525394a59e83c51b7b0c0df6ac2cdcb
Ali Al-Thaqbani <abu7alwah@gmail.com>
2026-07-10 19:53:32 +0300

    Add welcom screen  UI, assets, and optimize gdradle build - Add decorative background images to assets folder . - Created initial layout for the Welcome screen using Google Fonts . - Fixed Gradle memory allocation limits in gradle.properties .

--- lib/Screens/welcom.dart
+++ lib/Screens/welcom.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Welcom extends StatelessWidget {
   const Welcom({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Align(
+            alignment: Alignment.topRight,
+            child: Image.asset('assets/BG2.png',width: 350,),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text("Welcom",style: GoogleFonts.rubik(
+            textStyle: TextStyle(fontSize: 40,fontWeight: FontWeight.w600)
+          )
+          ),
+          ),
+          Padding(
+            padding:const EdgeInsets.all(16),
+          
+          child:Text('Lorm ipsum dolor sit amet, consectetur adipiscing elit. In euismod ipsum et dui rhoncus auctor.',style: GoogleFonts.rubik(
+            textStyle: TextStyle(fontSize: 16,fontWeight: FontWeight.w400,color: const Color.fromARGB(255, 132, 133, 134))
+          )
+          ),
+          )
+        ],
+      ),
+    );
   }
 }
