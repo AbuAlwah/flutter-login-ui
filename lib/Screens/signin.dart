b25f2fcea064075e31c35b463b8584e9c8be76d7
Ali Al-Thaqbani <abu7alwah@gmail.com>
2026-07-11 00:04:33 +0300

    Improve Signin screen UI with consistent padding and enhanced input fields for email and password

--- lib/Screens/signin.dart
+++ lib/Screens/signin.dart
@@ -16,7 +16,7 @@ class Signin extends StatelessWidget {
             child: Image.asset('assets/singleCircle.png', width: 250),
           ),
           Padding(
-            padding: EdgeInsetsGeometry.only(left: 30),
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
             child: Text(
               'Sign in',
               style: GoogleFonts.rubik(
@@ -26,11 +26,68 @@ class Signin extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsetsGeometry.only(left: 30),
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
             child: Image.asset('assets/Line.png', width: 95),
           ),
 
-          SizedBox(height: 60),
+          SizedBox(height: 50),
+          Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Text('Email', style: GoogleFonts.rubik(textStyle: TextStyle(fontWeight: FontWeight.w500))),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.email_outlined),
+                hintText: 'ali@gmail.com',hintStyle: TextStyle(color: const Color.fromARGB(255, 193, 193, 191)),
+                border: UnderlineInputBorder(),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Color(0xff6F12E7)),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: const Color.fromARGB(255, 186, 186, 186),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+          ),
+                    SizedBox(
+            height: 25,
+          ),
+           Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+            child: Column(
+              children: [
+                Text('Password', style: GoogleFonts.rubik(textStyle: TextStyle(fontWeight: FontWeight.w500))),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.password_outlined),
+                hintText: '*****',hintStyle: TextStyle(color: const Color.fromARGB(255, 193, 193, 191)),
+                border: UnderlineInputBorder(),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(width: 2, color: Color(0xff6F12E7)),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: const Color.fromARGB(255, 186, 186, 186),
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
